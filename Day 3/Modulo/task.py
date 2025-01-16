452f6818e254f24144cfe62a87214e96054da692
sayeedatabassum <itssayeeda@gmail.com>
2025-01-16 17:29:19 +0530

    day 3 - Modulo

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,14 @@
+number_to_check = int(input("what is the number you want to check\n"))
+
+if number_to_check % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+# what is the number you want to check
+# 3
+# odd
+
+# what is the number you want to check
+# 4
+# even
