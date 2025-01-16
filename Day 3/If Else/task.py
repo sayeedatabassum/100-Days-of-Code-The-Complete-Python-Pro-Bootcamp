47707bdb7d936bd24a0fbe39b2beb51efb130694
sayeedatabassum <itssayeeda@gmail.com>
2025-01-16 17:04:26 +0530

    day 3 - if else

---
+++ Day 3/If Else/task.py
@@ -0,0 +1,17 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height == 120:
+    print("You can ride the rollercoaster!")
+else:
+    print("Sorry, you didn't ride the rollercoaster!")
+
+# O/P:
+#
+# Welcome to the rollercoaster!
+# What is your height in cm? 50
+# Sorry, you didn't ride the rollercoaster!
+
+# Welcome to the rollercoaster!
+# What is your height in cm? 150
+# You can ride the rollercoaster!
